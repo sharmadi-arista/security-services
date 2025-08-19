6afff57392b9888f1f35e93d50c929fe731a7cc5
Marcus Hines <hines@google.com>
2025-08-19 20:57:29 +0000

    add more json rendering

--- cmd/sbom/sbom.go
+++ cmd/sbom/sbom.go
@@ -1,19 +1,53 @@
 package sbom
 
 import (
+	"encoding/json"
 	"fmt"
 
 	cdx "github.com/CycloneDX/cyclonedx-go"
+	"github.com/spdx/tools-golang/spdx"
 	log "k8s.io/klog"
 )
 
 func ConvertToGoogleSPDX(bom *cdx.BOM) (string, error) {
+	spdxDoc := spdx.Document{}
+	spdxDoc.DocumentName = bom.Metadata.Component.Name
 	refMap := map[string]cdx.Component{}
+	typeMap := map[string]int{}
 	for _, c := range *bom.Components {
 		if _, ok := refMap[c.BOMRef]; ok {
 			return "", fmt.Errorf("duplicate BOM ref: %q", c.BOMRef)
 		}
-		refMap[c.BOMRef] = c
+		typeMap[string(c.Type)] += 1
+		if c.Type == "library" {
+			refMap[c.BOMRef] = c
+			p := &spdx.Package{
+				PackageName:        c.Name,
+				PackageVersion:     c.Version,
+				PackageDescription: c.Description,
+			}
+			if c.PackageURL != "" {
+				p.PackageExternalReferences = append(p.PackageExternalReferences, &spdx.PackageExternalReference{
+					Category: "SECURITY",
+					Locator:  c.PackageURL,
+					RefType:  "purl",
+				})
+			}
+			if c.CPE != "" {
+				p.PackageExternalReferences = append(p.PackageExternalReferences, &spdx.PackageExternalReference{
+					Category: "SECURITY",
+					Locator:  c.CPE,
+					RefType:  "cpe22Type",
+				})
+			}
+			spdxDoc.Packages = append(spdxDoc.Packages, p)
+		}
+	}
+
+	for _, c := range *bom.Compositions {
+		if c.BOMRef != "" {
+			log.Infof("compos: %q", c.BOMRef)
+		}
 	}
 	refErrs := 0
 	log.Infof("Loaded %d components from BOM", len(refMap))
@@ -38,5 +72,10 @@ func ConvertToGoogleSPDX(bom *cdx.BOM) (string, error) {
 		log.Infof("%q: %q", c.Name, deps)
 	}
 	log.Infof("SBOM map ref errs: %d", refErrs)
-	return "", nil
+	b, err := json.MarshalIndent(spdxDoc, "", "  ")
+	if err != nil {
+		return "", err
+	}
+	log.Infof("TypeMap: %+v", typeMap)
+	return string(b), nil
 }
--- go.mod
+++ go.mod
@@ -9,8 +9,10 @@ require (
 
 require (
 	github.com/CycloneDX/cyclonedx-go v0.9.2 // indirect
+	github.com/anchore/go-struct-converter v0.0.0-20221118182256-c68fdcfa2092 // indirect
 	github.com/golang/glog v1.2.5 // indirect
 	github.com/inconshreveable/mousetrap v1.1.0 // indirect
+	github.com/spdx/tools-golang v0.5.5 // indirect
 	github.com/spf13/pflag v1.0.6 // indirect
 	k8s.io/klog v1.0.0 // indirect
 )
