13c7f24f42c146a5220cc7da5e78f894039c9c96
Marcus Hines <hines@google.com>
2025-08-20 13:14:20 +0000

    Added more error checking Added some assembly parsing

--- cmd/sbom/sbom.go
+++ cmd/sbom/sbom.go
@@ -26,12 +26,14 @@ func ConvertToGoogleSPDX(bom *cdx.BOM) (string, error) {
 				PackageVersion:     c.Version,
 				PackageDescription: c.Description,
 			}
+			validIdentifier := false
 			if c.PackageURL != "" {
 				p.PackageExternalReferences = append(p.PackageExternalReferences, &spdx.PackageExternalReference{
 					Category: "SECURITY",
 					Locator:  c.PackageURL,
 					RefType:  "purl",
 				})
+				validIdentifier = true
 			}
 			if c.CPE != "" {
 				p.PackageExternalReferences = append(p.PackageExternalReferences, &spdx.PackageExternalReference{
@@ -39,16 +41,43 @@ func ConvertToGoogleSPDX(bom *cdx.BOM) (string, error) {
 					Locator:  c.CPE,
 					RefType:  "cpe22Type",
 				})
+				validIdentifier = true
+			}
+			if !validIdentifier {
+				log.Warningf("package %q missing PURL and CPE", c.Name)
 			}
 			spdxDoc.Packages = append(spdxDoc.Packages, p)
 		}
 	}
 
+	compTypeMap := map[string]int{}
+	assemblyDAG := map[string][]string{}
+	asmRefErr := 0
 	for _, c := range *bom.Compositions {
+		compType := "unknown"
+		switch {
+		case len(*c.Assemblies) != 0:
+			compType = "assembly"
+			// Let's parse the assembly
+			asmKey := string((*c.Assemblies)[0])
+			if _, ok := refMap[asmKey]; !ok {
+				asmRefErr++
+			}
+			v := assemblyDAG[asmKey]
+			for _, asm := range (*c.Assemblies)[1:] {
+				v = append(v, string(asm))
+			}
+		case len(*c.Dependencies) != 0:
+			compType = "dependency"
+		case len(*c.Vulnerabilities) != 0:
+			compType = "vulnerability"
+		}
+		compTypeMap[compType] += 1
 		if c.BOMRef != "" {
 			log.Infof("compos: %q", c.BOMRef)
 		}
 	}
+	log.Infof("Found %d Assembly ref errors", asmRefErr)
 	refErrs := 0
 	log.Infof("Loaded %d components from BOM", len(refMap))
 	log.Infof("building SBOM map")
@@ -56,11 +85,11 @@ func ConvertToGoogleSPDX(bom *cdx.BOM) (string, error) {
 		c, ok := refMap[rm.Ref]
 		if !ok {
 			refErrs++
-			log.Warningf("missing component ref: %q", rm.Ref)
+			log.V(1).Infof("missing component ref: %q", rm.Ref)
 			continue
 			// return "", fmt.Errorf("missing component ref: %q", rm.Ref)
 		}
-		log.Infof("building deps for component %q", c.Name)
+		log.V(1).Infof("building deps for component %q", c.Name)
 		var deps []string
 		for _, depRef := range *rm.Dependencies {
 			r, ok := refMap[depRef]
@@ -69,7 +98,7 @@ func ConvertToGoogleSPDX(bom *cdx.BOM) (string, error) {
 			}
 			deps = append(deps, r.Name)
 		}
-		log.Infof("%q: %q", c.Name, deps)
+		log.V(1).Infof("%q: %q", c.Name, deps)
 	}
 	log.Infof("SBOM map ref errs: %d", refErrs)
 	b, err := json.MarshalIndent(spdxDoc, "", "  ")
@@ -77,5 +106,6 @@ func ConvertToGoogleSPDX(bom *cdx.BOM) (string, error) {
 		return "", err
 	}
 	log.Infof("TypeMap: %+v", typeMap)
+	log.Infof("CompMap: %+v", compTypeMap)
 	return string(b), nil
 }
