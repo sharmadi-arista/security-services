ec90c194272846d676b2e0e721bfb5c31274130b
Divyansh Sharma <sharmadi@arista.com>
2025-09-17 12:49:55 -0700

    CDXTOSPDX: Document Creation Info and code-cleanup

--- cli/cmd/sbom/sbom.go
+++ cli/cmd/sbom/sbom.go
@@ -3,8 +3,10 @@ package sbom
 import (
 	"encoding/json"
 	"fmt"
+	"strings"
 
 	cdx "github.com/CycloneDX/cyclonedx-go"
+	"github.com/google/uuid"
 	"github.com/spdx/tools-golang/spdx"
 	"github.com/spdx/tools-golang/spdx/v2/common"
 	"github.com/spdx/tools-golang/spdx/v2/v2_3"
@@ -13,32 +15,50 @@ import (
 
 func ConvertToGoogleSPDX(bom *cdx.BOM) (*spdx.Document, error) {
 	spdxDoc := spdx.Document{
-		DocumentName:      bom.Metadata.Component.Name,
-		DocumentNamespace: "", // This needs to be a PURL?
-		SPDXVersion:       spdx.Version,
-		DataLicense:       "CC0-1.0",
-		CreationInfo: &v2_3.CreationInfo{
-			Created: bom.Metadata.Timestamp,
-			Creators: []common.Creator{{
-				Creator:     bom.Metadata.Component.Supplier.Name,
-				CreatorType: "Organization",
-			}},
-		},
+		SPDXVersion:    spdx.Version,
+		DataLicense:    "CC0-1.0",
 		SPDXIdentifier: toSPDXElementID("DOCUMENT"),
 	}
 
 	refMap := map[string]cdx.Component{}
 	typeMap := map[string]int{}
 
-	if bom.Metadata != nil && bom.Metadata.Component != nil {
-		if err := AddCycloneDXComponent(
-			*bom.Metadata.Component,
-			refMap,
-			typeMap,
-			&spdxDoc,
-		); err != nil {
-			return nil, fmt.Errorf("failed to add metadata component: %w", err)
+	if bom.Metadata != nil {
+		spdxDoc.CreationInfo = &v2_3.CreationInfo{
+			Created: bom.Metadata.Timestamp,
 		}
+
+		if bom.Metadata.Component != nil {
+			spdxDoc.DocumentName = bom.Metadata.Component.Name
+			if bom.Metadata.Component.Supplier != nil {
+				spdxDoc.CreationInfo.Creators = []common.Creator{{
+					Creator:     bom.Metadata.Component.Supplier.Name,
+					CreatorType: "Organization",
+				}}
+			}
+
+			if err := AddCycloneDXComponent(
+				*bom.Metadata.Component,
+				refMap,
+				typeMap,
+				&spdxDoc,
+			); err != nil {
+				return nil, fmt.Errorf("failed to add metadata component: %w", err)
+			}
+		}
+	}
+
+	if spdxDoc.DocumentNamespace == "" {
+		var docID string
+		if bom.SerialNumber == "" {
+			docID = uuid.New().String()
+		} else if strings.HasPrefix(bom.SerialNumber, "urn:uuid:") {
+			docID = strings.TrimPrefix(bom.SerialNumber, "urn:uuid:")
+		} else {
+			docID = bom.SerialNumber
+		}
+		newNamespace := fmt.Sprintf("http://spdx.org/spdxdocs/%s-%s", spdxDoc.DocumentName, docID)
+		spdxDoc.DocumentNamespace = newNamespace
 	}
 
 	if bom.Components != nil {
@@ -49,59 +69,6 @@ func ConvertToGoogleSPDX(bom *cdx.BOM) (*spdx.Document, error) {
 		}
 	}
 
-	compTypeMap := map[string]int{}
-	assemblyDAG := map[string][]string{}
-	asmRefErr := 0
-	for _, c := range *bom.Compositions {
-		compType := "unknown"
-		switch {
-		case len(*c.Assemblies) != 0:
-			compType = "assembly"
-			// Let's parse the assembly
-			asmKey := string((*c.Assemblies)[0])
-			if _, ok := refMap[asmKey]; !ok {
-				asmRefErr++
-			}
-			v := assemblyDAG[asmKey]
-			for _, asm := range (*c.Assemblies)[1:] {
-				v = append(v, string(asm))
-			}
-		case len(*c.Dependencies) != 0:
-			compType = "dependency"
-		case len(*c.Vulnerabilities) != 0:
-			compType = "vulnerability"
-		}
-		compTypeMap[compType] += 1
-		if c.BOMRef != "" {
-			log.Infof("compos: %q", c.BOMRef)
-		}
-	}
-	log.Infof("Found %d Assembly ref errors", asmRefErr)
-	refErrs := 0
-	log.Infof("Loaded %d components from BOM", len(refMap))
-	log.Infof("building SBOM map")
-	for _, rm := range *bom.Dependencies {
-		c, ok := refMap[rm.Ref]
-		if !ok {
-			refErrs++
-			log.V(1).Infof("missing component ref: %q", rm.Ref)
-			continue
-			// return "", fmt.Errorf("missing component ref: %q", rm.Ref)
-		}
-		log.V(1).Infof("building deps for component %q", c.Name)
-		var deps []string
-		for _, depRef := range *rm.Dependencies {
-			r, ok := refMap[depRef]
-			if !ok {
-				log.V(1).Infof("missing dep component ref: %q", rm.Ref)
-			}
-			deps = append(deps, r.Name)
-		}
-		log.V(1).Infof("%q: %q", c.Name, deps)
-	}
-	log.Infof("SBOM map ref errs: %d", refErrs)
-	log.Infof("TypeMap: %+v", typeMap)
-	log.Infof("CompMap: %+v", compTypeMap)
 	return &spdxDoc, nil
 }
 
